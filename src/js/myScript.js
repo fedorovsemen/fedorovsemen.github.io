2803c294f3181a217b926a13ae16f34caae9d21c
fedorovsemen <104171269+fedorovsemen@users.noreply.github.com>
2022-10-07 22:31:45 +0300

    Add files via upload

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,29 +1,6 @@
-$(window).load(function() {
- 
-  $(".loader_inner").fadeOut();
-  $(".loader").delay(400).fadeOut("slow");
- 
-});
-
-
-
+$(document).ready(function(){
 	
 	
-	
-
-
-		
-
-	
-	
-	
-
-
-
-
-
-	
-
 $("#inputTel").mask("+7(999) 999-9999");
 
 	$('form').submit(function(event){
@@ -32,6 +9,86 @@ $("#inputTel").mask("+7(999) 999-9999");
 			alert("Ведитe поле");
 		}
 	})
+	
+	
+	$('a[href^="#contacts"]').click(function () {
+	let valHref = $(this).attr("href");
+	$('html, body').animate({
+		scrollTop: $(valHref).offset().top +150 + "px"
+	});
+});
+	
+	
+	$('a[href^="#case"]').click(function () {
+	let valHref = $(this).attr("href");
+	$('html, body').animate({
+		scrollTop: $(valHref).offset().top -150 + "px"
+	});
+});
+	
+	
+	$('a[href^="#about_me"]').click(function () {
+	let valHref = $(this).attr("href");
+	$('html, body').animate({
+		scrollTop: $(valHref).offset().top -60 + "px"
+	});
 
+	
+	
+	
+$(window).scroll(() =>{
+            let scrollDistance = $(window).scrollTop();
+        $(".section").each((i, el) => {
+                if ($(el).offset().top - $("nav").outerHeight() <= scrollDistance) {
+                    $("nav a").each((i, el) => {
+                        if ($(el).hasClass("active")) {
+                            $(el).removeClass("active");
+                        }
+                    });
+                    $('nav li:eq('+ i +')').find('a').addClass('active'); 
+                }
+        });
+});
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+});
 
+	
 
